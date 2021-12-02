6b9ddd8a14d884a4b3d5b7f3c30875e0e83c3ebf
Thais Kotovicz <thais_kotovicz@hotmail.com>
2021-12-02 15:40:35 -0300

    Requisito 1 ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(darthVader, lukeSkywalker) {
+  if (darthVader === true && lukeSkywalker === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
